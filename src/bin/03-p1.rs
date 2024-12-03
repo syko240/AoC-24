e573ded8993670cedaacaf986f2ba5a2548f9f6d
syko240 <andrepost@hotmail.se>
2024-12-03 07:19:06 +0100

    d3

--- src/bin/03-p1.rs
+++ src/bin/03-p1.rs
@@ -1,3 +1,5 @@
+use regex::Regex;
+
 fn main() {
     let input = include_str!("./input.txt");
     let output = aoc_solve(input);
@@ -5,7 +7,13 @@ fn main() {
 }
 
 fn aoc_solve(input: &str) -> String {
-    todo!()
+    let mul_re = Regex::new(r"mul\(([0-9]{1,3}),([0-9]{1,3})\)").unwrap();
+    let mut sum = 0;
+
+    for cap in mul_re.captures_iter(input) {
+        sum += cap[1].parse::<i32>().unwrap() * cap[2].parse::<i32>().unwrap();
+    }
+    sum.to_string()
 }
 
 #[cfg(test)]
@@ -14,7 +22,7 @@ mod tests {
 
     #[test]
     fn pass() {
-        let result = aoc_solve("");
-        assert_eq!(result, "".to_string());
+        let result = aoc_solve("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))");
+        assert_eq!(result, "161".to_string());
     }
 }
--- src/bin/03-p2.rs
+++ src/bin/03-p2.rs
@@ -1,3 +1,5 @@
+use regex::Regex;
+
 fn main() {
     let input = include_str!("./input.txt");
     let output = aoc_solve(input);
@@ -5,7 +7,33 @@ fn main() {
 }
 
 fn aoc_solve(input: &str) -> String {
-    todo!()
+    let vec = parse(input);
+    let mut sum = 0;
+    let mut skip = false;
+
+    for val in vec {
+        if val == "do()" {
+            skip = false;
+        } else if val == "don't()" {
+            skip = true;
+        } else {
+            if skip {
+                continue;
+            }
+            let operands: Vec<&str> = val[4..val.len()-1].split(',').collect();
+            sum += operands[0].parse::<i32>().unwrap() * operands[1].parse::<i32>().unwrap();
+        }
+    }
+    sum.to_string()
+}
+
+fn parse(input: &str) -> Vec<&str> {
+    let mul_re = Regex::new(r"mul\(([0-9]{1,3}),([0-9]{1,3})\)|do\(\)|don't\(\)").unwrap();
+    let mut vec = Vec::new();
+    for mat in mul_re.find_iter(input) {
+        vec.push(mat.as_str());
+    }
+    vec
 }
 
 #[cfg(test)]
@@ -14,7 +42,7 @@ mod tests {
 
     #[test]
     fn pass() {
-        let result = aoc_solve("");
-        assert_eq!(result, "".to_string());
+        let result = aoc_solve("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))");
+        assert_eq!(result, "48".to_string());
     }
 }
