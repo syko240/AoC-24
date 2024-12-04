94fd70b7edfcc26e3bbbb3340f675786a9cacc5a
syko240 <andrepost@hotmail.se>
2024-12-04 12:54:15 +0100

    d4

--- src/bin/04-p1.rs
+++ src/bin/04-p1.rs
@@ -5,7 +5,41 @@ fn main() {
 }
 
 fn aoc_solve(input: &str) -> String {
-    todo!()
+    let map = parse(input);
+    let w = "XMAS";
+    let dirs = [(1, 1), (-1, -1), (1, 0), (0, 1), (-1, 0), (0, -1), (-1, 1), (1, -1)];
+
+    let mut count = 0;
+    for x in 0..map.len() as isize {
+        for y in 0..map[0].len() as isize {
+            for &(dx, dy) in &dirs {
+                count += lookup(&map, w, x, y, dx, dy);
+            }
+        }
+    }
+    count.to_string()
+}
+
+fn lookup(map: &Vec<Vec<char>>, w: &str, mut x: isize, mut y: isize, dx: isize, dy: isize) -> usize {
+    let chars: Vec<char> = w.chars().collect();
+
+    for idx in 0..chars.len() as isize {
+        if x < 0 || x >= map.len() as isize || y < 0 || y >= map[0].len() as isize {
+            return 0;
+        }
+        if map[x as usize][y as usize] != chars[idx as usize] {
+            return 0;
+        }
+        x += dx;
+        y += dy;
+    }
+    1
+}
+
+fn parse(input: &str) -> Vec<Vec<char>> {
+    input.lines()
+        .map(|line| line.chars().collect())
+        .collect()
 }
 
 #[cfg(test)]
@@ -14,7 +48,17 @@ mod tests {
 
     #[test]
     fn pass() {
-        let result = aoc_solve("");
-        assert_eq!(result, "".to_string());
+        let result = aoc_solve("\
+        MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX");
+        assert_eq!(result, "18".to_string());
     }
 }
--- src/bin/04-p2.rs
+++ src/bin/04-p2.rs
@@ -5,7 +5,35 @@ fn main() {
 }
 
 fn aoc_solve(input: &str) -> String {
-    todo!()
+    let map = parse(input);
+    let w = "MAS";
+
+    let mut count = 0;
+    for x in 1..map.len() -1 {
+        for y in 1..map[0].len() - 1 {
+            count += lookup(&map, w, x, y);
+        }
+    }
+    count.to_string()
+}
+
+fn lookup(map: &Vec<Vec<char>>, w: &str, x: usize, y: usize) -> usize {
+    let w_rev: Vec<char> = w.chars().rev().collect();
+
+    let left_right = vec![ map[x-1][y-1], map[x][y], map[x+1][y+1] ];
+    let right_left = vec![ map[x-1][y+1], map[x][y], map[x+1][y-1] ];
+
+    if (left_right == w.chars().collect::<Vec<_>>() || left_right == w_rev) && (right_left == w.chars().collect::<Vec<_>>() || right_left == w_rev) {
+        1
+    } else {
+        0
+    }
+}
+
+fn parse(input: &str) -> Vec<Vec<char>> {
+    input.lines()
+        .map(|line| line.chars().collect())
+        .collect()
 }
 
 #[cfg(test)]
@@ -14,7 +42,17 @@ mod tests {
 
     #[test]
     fn pass() {
-        let result = aoc_solve("");
-        assert_eq!(result, "".to_string());
+        let result = aoc_solve("\
+        .M.S......
+..A..MSMS.
+.M.S.MAA..
+..A.ASMSM.
+.M.S.M....
+..........
+S.S.S.S.S.
+.A.A.A.A..
+M.M.M.M.M.
+..........");
+        assert_eq!(result, "9".to_string());
     }
 }
